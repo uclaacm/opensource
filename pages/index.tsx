4a5f27ef24b0af82910573f32b028941a435e00e
Matt Wang <matt@matthewwang.me>
2021-05-05 02:33:20 -0700

    trivial octokit example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Octokit } from "@octokit/core";
 import Layout from '../components/Layout';
 
 function Home(): JSX.Element {
+  const [acmRepos, setACMRepos] = useState(0);
+
+  useEffect(()=> {
+    // TODO(mattxwang): change the auth scope and get members, etc.
+    // see: https://docs.github.com/en/rest/reference/orgs
+    const octokit = new Octokit();
+    async function getGHRepoCounts() {
+      const response = await octokit.request("GET /orgs/{org}", {
+        org: "uclaacm",
+      })
+      setACMRepos(response.data.public_repos);
+    }
+    getGHRepoCounts();
+  }, [])
+
   return (
   <Layout>
     <div className="container">
       <h1 className="title">
         opensource at <a href="https://uclaacm.com">ACM at UCLA</a>
       </h1>
+
+      <p>
+        repositories: {acmRepos}
+      </p>
 
       <div className="grid">
         <a href="https://nextjs.org/docs" className="card">
