a6e63cd7908da9eae1c2df5c51805bf87d6b9f6a
Matt Wang <matt@matthewwang.me>
2021-07-11 20:22:18 -0700

    content: adds external links to footer for acm, next

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ELink from '../components/ELink';
 
 function Footer(): JSX.Element {
   return (
     <footer>
       <div className="container">
-        made by acm at ucla, with next
+        made by <ELink link="https://uclaacm.com">ACM at UCLA</ELink>, with <ELink link="https://nextjs.org/docs">Next.js</ELink>
       </div>
     </footer>
   );
