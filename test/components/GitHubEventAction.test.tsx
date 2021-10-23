750209f59815a42f14c00fc8668f5bb4274a0fec
Ansh Saini <anshsaini625@gmail.com>
2021-10-23 10:09:17 +0530

    update test to use fixtures

--- test/components/GitHubEventAction.test.tsx
+++ test/components/GitHubEventAction.test.tsx
@@ -1,148 +1,74 @@
 import React from 'react';
 import GitHubEventAction from '../../components/GitHubEventAction';
+import createEventData from '../fixtures/createEvent.json';
+import deleteEventData from '../fixtures/deleteEvent.json';
+import forkEventData from '../fixtures/forkEvent.json';
+import issueCommentEventData from '../fixtures/issueCommentEvent.json';
+import issuesEventData from '../fixtures/issuesEvent.json';
+import pullRequestEventData from '../fixtures/pullRequestEvent.json';
+import pullRequestReviewCommentEventData from '../fixtures/pullRequestReviewCommentEvent.json';
+import pullRequestReviewEventData from '../fixtures/pullRequestReviewEvent.json';
+import pushEventData from '../fixtures/pushEvent.json';
+
 import { render } from '../testUtils';
+
 
 const testCases = [
   {
-    data: {
-      type: 'CreateEvent',
-      payload: {
-        ref: 'dependabot/npm_and_yarn/eslint-plugin-import-2.25.2',
-        ref_type: 'branch',
-      },
-    },
-    expectedOutput: 'created branch dependabot/npm_and_yarn/eslint-plugin-import-2.25.2 in',
+    data: createEventData,
+    expectedOutput: 'created branch town-hall-f21 in',
   },
   {
-    data: {
-      type: 'DeleteEvent',
-      payload: {
-        ref: 'dependabot/npm_and_yarn/eslint-plugin-import-2.25.2',
-        ref_type: 'branch',
-      },
-    },
-    expectedOutput: 'deleted branch dependabot/npm_and_yarn/eslint-plugin-import-2.25.2 in',
+    data: deleteEventData,
+    expectedOutput: 'deleted branch jedis-typo in',
   },
   {
-    data: {
-      type: 'ForkEvent',
-      payload: {
-        forkee: {
-          full_name: 'ansh-saini/opensource',
-          html_url: 'https://github.com/ansh-saini/opensource',
-        },
-      },
-    },
-    expectedOutput: 'forked ansh-saini/opensource from',
+    data: forkEventData,
+    expectedOutput: 'forked ansh-saini/website-1 from',
+    expectedHref: 'https://github.com/ansh-saini/website-1',
   },
   {
-    data: {
-      type: 'IssueCommentEvent',
-      payload: {
-        action: 'created',
-        issue: {
-          html_url: 'https://github.com/uclaacm/opensource/issues/14',
-          number: 14,
-        },
-      },
-    },
-    expectedOutput: 'commented on issue #14 in',
-    testContext: 'create',
+    data: issueCommentEventData,
+    expectedOutput: 'commented on issue #45 in',
+    expectedHref: 'https://github.com/uclaacm/membership-portal/pull/45',
   },
   {
-    data: {
-      type: 'IssueCommentEvent',
-      payload: {
-        action: 'closed',
-        issue: {
-          html_url: 'https://github.com/uclaacm/opensource/issues/14',
-          number: 14,
-        },
-      },
-    },
-    expectedOutput: 'closed issue #14 in',
-    testContext: 'close',
+    data: issuesEventData,
+    expectedOutput: 'opened issue #106 in',
+    expectedHref: 'https://github.com/uclaacm/icpc-website/issues/106',
   },
   {
-    data: {
-      type: 'IssueCommentEvent',
-      payload: {
-        action: 'closed',
-        issue: {
-          html_url: 'https://github.com/uclaacm/opensource/issues/14',
-        },
-      },
-    },
-    expectedOutput: 'closed an issue in',
-    testContext: 'without issue number',
+    data: pullRequestEventData,
+    expectedOutput: 'opened pull request #324 in',
+    expectedHref: 'https://github.com/uclaacm/website/pull/324',
   },
   {
-    data: {
-      type: 'IssuesEvent',
-      payload: {
-        action: 'closed',
-        issue: {
-          html_url: 'https://github.com/uclaacm/opensource/issues/14',
-          number: 14,
-        },
-      },
-    },
-    expectedOutput: 'closed issue #14 in',
+    data: pullRequestReviewEventData,
+    expectedOutput: 'reviewed pull request #262 in',
+    expectedHref: 'https://github.com/uclaacm/hack.uclaacm.com/pull/262',
   },
   {
-    data: {
-      type: 'PullRequestEvent',
-      payload: {
-        action: 'opened',
-        number: 644,
-        pull_request: {
-          html_url: 'https://github.com/uclaacm/TeachLAFrontend/pull/644',
-        },
-      },
-    },
-    expectedOutput: 'opened pull request #644 in',
+    data: pullRequestReviewCommentEventData,
+    expectedOutput: 'commented on pull request #262 in',
+    expectedHref: 'https://github.com/uclaacm/hack.uclaacm.com/pull/262#discussion_r734235143',
   },
   {
-    data: {
-      type: 'PullRequestReviewEvent',
-      payload: {
-        action: 'created',
-        pull_request: {
-          html_url: 'https://github.com/uclaacm/TeachLAFrontend/pull/642',
-          number: 642,
-        },
-      },
-    },
-    expectedOutput: 'reviewed pull request #642 in',
-  },
-  {
-    data: {
-      type: 'PushEvent',
-      payload: {
-        size: 1,
-      },
-    },
+    data: pushEventData,
     expectedOutput: 'pushed 1 commit to',
-    testContext: 'single commits',
   },
-  {
-    data: {
-      type: 'PushEvent',
-      payload: {
-        size: 2,
-      },
-    },
-    expectedOutput: 'pushed 2 commits to',
-    testContext: 'plural commits',
-  },
-
 ];
 
 describe('GitHub Event Action', () => {
-  testCases.forEach(({data ,expectedOutput, testContext}) => {
-    it(`renders correct string for ${data.type}${testContext ? ` (${testContext})` : ''}`, () => {
+  testCases.forEach(({data ,expectedOutput, expectedHref}) => {
+    it(`renders correct string for ${data.type}}`, () => {
       const {container} = render(<GitHubEventAction payload={data.payload} type={data.type} />, {});
-      expect(container.querySelector('span').textContent).toBe(expectedOutput);
+
+      const element = container.querySelector('span');
+      expect(element.textContent).toBe(expectedOutput);
+
+      if (expectedHref) {
+        expect(element.querySelector('a').href).toBe(expectedHref);
+      }
     });
   });
 });
