986da432aa663e6ba5e655dec075f075b527511f
Jack Lin <78662867+JackTheVac@users.noreply.github.com>
2022-01-06 23:10:58 -0800

    Added Time Stamp to GitHub Events using Moment.js (#70)
    * added timestamp
    
    * added Timestamp with lint fix
    
    * cleaning up
    
    * cleaning up pt2
    
    * Delete Spaces
    
    Co-authored-by: Regina Wang <rrreggginnna@gmail.com>
    
    * final cleanup
    
    * clean up eslint
    
    * cleaning up yarn.lock
    
    * cleaning up rest
    
    * node update
    
    * Update components/GitHubEvent.tsx
    
    Co-authored-by: Regina Wang <rrreggginnna@gmail.com>
    
    * Update components/GitHubEventAction.tsx
    
    Co-authored-by: Regina Wang <rrreggginnna@gmail.com>
    
    Co-authored-by: Regina Wang <rrreggginnna@gmail.com>

--- components/GitHubEvent.tsx
+++ components/GitHubEvent.tsx
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import React from 'react';
 import ELink from './ELink';
 import GitHubEventAction from './GitHubEventAction';
@@ -29,14 +30,19 @@ interface GitHubRepo {
 }
 
 function GitHubEvent(props: GitHubEvent): JSX.Element {
-  const {type, actor, repo, payload} = props;
+  const {type, actor, repo, payload, created_at} = props;
+  const timePassed = moment(created_at).fromNow();
+
   const userLink = !actor.login.includes('[bot]') ? <ELink link={`https://github.com/${actor.login}`}>{`@${actor.login}`}</ELink> : actor.login;
   return (
     <>
       {/* <div className="card" style={{marginTop: "20px"}}> */}
       {/* <div className="card-body"> */}
-      {userLink} <GitHubEventAction type={type} payload={payload} />{' '}
-      <ELink link={`https://github.com/${repo.name}`}>{repo.name}</ELink>
+      <div style = {{overflow: 'hidden'}}>
+        {userLink} <GitHubEventAction type={type} payload={payload}/>{' '}
+        <ELink link={`https://github.com/${repo.name}`}>{repo.name}</ELink>
+        <span style = {{float: 'right'}}>{timePassed}</span>
+      </div>
       <hr />
       {/*  </div> */}
       {/* // </div> */}
