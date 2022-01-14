97d0f6984ee622451f27bcfb95f80c6cafe8e1b6
Arush Ramteke <arushramtek@gmail.com>
2022-01-13 18:30:23 -0800

    Initial addition of next-seo, not tested yet

--- pages/contribute.tsx
+++ pages/contribute.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { NextSeo } from 'next-seo';
 import Layout from '../components/Layout';
 
 function Contribute(): JSX.Element {
   return (
     <Layout>
       <div className="container">
+        <NextSeo
+          title="contribute | open source at ACM at UCLA"
+          description="get started by contributing to one of our projects"
+          openGraph={{
+            images: [{
+              url: 'https://www.uclaacm.com/images/logo.png',
+              width: 1200,
+              height: 1200,
+              alt: 'The ACM at UCLA logo',
+            }],
+            site_name: 'open source at ACM at UCLA'
+          }}
+        />
         <h1>
           get started
         </h1>
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NextSeo } from 'next-seo';
 import Layout from '../components/Layout';
 import ProjectCard from '../components/ProjectCard';
 
@@ -8,6 +9,19 @@ function Projects(): JSX.Element {
   return (
     <Layout>
       <div className="container">
+        <NextSeo
+          title="projects | open source at ACM at UCLA"
+          description="a heads-up overview of the projects that power ACM at UCLA"
+          openGraph={{
+            images: [{
+              url: 'https://www.uclaacm.com/images/logo.png',
+              width: 1200,
+              height: 1200,
+              alt: 'The ACM at UCLA logo',
+            }],
+            site_name: 'open source at ACM at UCLA'
+          }}
+        />
         <h1>
           projects
         </h1>
