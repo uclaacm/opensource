bf446e148dbcc77979e9a9b8a6acca805cebe905
Mirayna Mckinney <miraynamckinney77@gmail.com>
2022-01-18 18:59:55 -0800

    Mock random 0
    Used sinon to mock math.random.

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import sinon from 'sinon';
 import Home from '../../pages/index';
 import { render } from '../testUtils';
 
 describe('Home page', () => {
   it('matches snapshot', () => {
+    sinon.stub(Math, 'random').returns(0);
     const { asFragment } = render(<Home numRepos={0} recentEvents={[]} />, {});
     expect(asFragment()).toMatchSnapshot();
   });
