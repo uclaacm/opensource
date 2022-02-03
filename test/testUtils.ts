8b70f3301936cfac269760bc0fbdc325ffe7ef21
Matt Wang <matt@matthewwang.me>
2022-02-02 17:13:21 -0800

    resolves more complicated type errors

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -1,23 +1,10 @@
 import { render } from '@testing-library/react';
-// import { ThemeProvider } from "my-ui-lib"
-// import { TranslationProvider } from "my-i18n-lib"
-// import defaultStrings from "i18n/en-x-default"
-
-const Providers = ({ children }) => {
-  return children;
-  // return (
-  //   <ThemeProvider theme="light">
-  //     <TranslationProvider messages={defaultStrings}>
-  //       {children}
-  //     </TranslationProvider>
-  //   </ThemeProvider>
-  // )
-};
+import React from 'react';
 
 // boilerplate code
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const customRender = (ui: JSX.Element, options = {}): any =>
-  render(ui, { wrapper: Providers, ...options });
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const customRender = (ui: React.ReactElement, options = {}) =>
+  render(ui, { wrapper: ({children}) => children, ...options });
 
 // re-export everything
 export * from '@testing-library/react';
