b801d951818befec0d84811a2bf56cee992f595a
Matt Wang <matt@matthewwang.me>
2022-02-02 17:16:19 -0800

    adds ESLint Next plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@next/next/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
